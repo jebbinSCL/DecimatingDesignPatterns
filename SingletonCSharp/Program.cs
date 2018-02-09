f4d63d186b43e949fffc94984018c3902b59cc67
Jason Ebbin <jebbin@scottlogic.co.uk>
2018-02-09 16:40:59 +0000

    Add InitialSolution with CSharp Singleton pattern

---
+++ SingletonCSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SingletonCSharp
+{
+    static class RandomUser
+    {
+        public static void WriteRandomNumbers()
+        {
+            var random = GlobalRandom.GetInstance();
+            Console.WriteLine(random.Next());
+
+            var sameRandom = GlobalRandom.GetInstance();
+            Console.WriteLine(random.Next());
+
+            var randomsAreTheSame = ReferenceEquals(random, sameRandom);
+            Console.WriteLine($"Randoms are the same: {randomsAreTheSame}");
+        }
+    }
+
+    class GlobalRandom : Random
+    {
+        private static GlobalRandom instance = new GlobalRandom(11123123);
+        private GlobalRandom(int seed) : base(seed) { }
+        public static GlobalRandom GetInstance()
+        {
+            return instance;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomUser.WriteRandomNumbers();
+            Console.ReadLine();
+        }
+    }
+}
